4870f9b38100c2c9b2d0bb828405e29cdc0f5a16
yana-hris <yana.hristova.work@gmail.com>
2025-01-30 15:03:28 +0200

    Removed bin/ and obj/ files from Git and updated .gitignore

---
+++ RecipeClient/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace RecipeClient
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            RecipeServiceClient client = new RecipeServiceClient();
+
+            Console.WriteLine("Fetching recipes...");
+
+            var recipes = client.GetRecipes();
+
+
+            foreach (var recipe in recipes)
+            {
+                Console.WriteLine("\n=====================================");
+                Console.WriteLine($" RECIPE: {recipe.Name}");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($" Description: {recipe.Description}");
+                Console.WriteLine($" Image URL: {recipe.ImageUrl}");
+                Console.WriteLine("\n INGREDIENTS:");
+
+                foreach (var ingredient in recipe.Ingredients)
+                {
+                    Console.WriteLine($"   - {ingredient}");
+                }
+
+                Console.WriteLine("\n INSTRUCTIONS:");
+                Console.WriteLine($"{recipe.Instructions}");
+                Console.WriteLine("=====================================\n");
+            }
+
+            // Call GetRecipeById(1)
+            Console.WriteLine("\nFetching recipe with ID 1...");
+            var recipeById = client.GetRecipeById(Guid.Parse("c0420eee-5291-41a5-bc00-8db342e0ccc0"));
+            Console.WriteLine($"{recipeById.Id}: {recipeById.Name} - {string.Join(", ", recipeById.Ingredients)}");
+
+            // Close client connection
+            client.Close();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RecipeWcfHost/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace RecipeWcfHost
+{
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+
+    using RecipeWcfService;
+
+    class Program
+    {
+        static void Main()
+        {
+            Uri baseAddress = new Uri("http://localhost:9000/RecipeService");
+
+            using (ServiceHost host = new ServiceHost(typeof(RecipeService), baseAddress))
+            {
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true; // Enable WSDL metadata
+                smb.HttpGetUrl = baseAddress;
+                host.Description.Behaviors.Add(smb);
+
+                host.AddServiceEndpoint(
+                    ServiceMetadataBehavior.MexContractName,
+                    MetadataExchangeBindings.CreateMexHttpBinding(), 
+                    "mex");
+
+                host.AddServiceEndpoint(typeof(IRecipeService), new BasicHttpBinding(), "");
+
+                host.Open();
+                Console.WriteLine("WCF SOAP сървисът е стартиран на:" + baseAddress);
+                Console.WriteLine("Натиснете Enter за спиране...");
+                Console.ReadLine();
+
+                host.Close();
+            }
+
+        }
+    }
+}
